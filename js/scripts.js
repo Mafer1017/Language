317dfb7b2ec2bcc5745b257071b1c7daa5587b46
Marcus <marcusferreira23@gmail.com>
2022-01-28 19:55:49 -0800

    added else if equations in js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
-  $("form#formOne").submit(function() {
+  $("form#surveyOne").submit(function() {
     const number1 = parseInt($("select#beverage").val());
     const number2 = parseInt($("select#music").val());
     const number3 = parseInt($("select#movie").val());
     const number4 = parseInt($("select#sport").val());
     const number5 = parseInt($("select#relax").val());
-    const result = $(number1 + number2 + number3 + number4 + number5) ;
+    const result = number1 + number2 + number3 + number4 + number5;
 
 
     
-    if (number1 === 1) {
+    if (result < 10) {
       $('#html').show();
-    } else if (number1 === 2) {
+    } else if (result  === 10) {
       $('#css').show();
-    } else if (number1 === 3) {
+    } else if (result > 10) {
       $('#javaScript').show();
     }
-    
     event.preventDefault();
   });
 });
