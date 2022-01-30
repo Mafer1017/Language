bc86df0c540e4a50a188e9e34acc5eb137fee270
Marcus <marcusferreira23@gmail.com>
2022-01-29 18:10:25 -0800

    minor tweaking in HTML/CSS/JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("form#surveyOne").submit(function() {
-    const number1 = parseInt($("select#beverage").val());
+    const number1 = parseInt($("select#drink").val());
     const number2 = parseInt($("select#music").val());
     const number3 = parseInt($("select#movie").val());
     const number4 = parseInt($("select#sport").val());
